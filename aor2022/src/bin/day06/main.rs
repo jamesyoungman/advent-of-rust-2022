ada8e9891ca12e9e7b2511cba40685d2aa5cb30d
James Youngman <james@youngman.org>
2022-12-06 06:09:09 +0000

    Solved Day 06 part 2.

--- aor2022/src/bin/day06/main.rs
+++ aor2022/src/bin/day06/main.rs
@@ -21,12 +21,16 @@ fn test_all_differ() {
     assert!(!all_differ(&['a', 'b', 'c', 'c']));
 }
 
-fn detect_sop(s: &str) -> Option<usize> {
+fn detect_start_indicator(s: &str, n: usize) -> Option<usize> {
     let v: Vec<char> = s.chars().collect();
-    v.windows(4)
+    v.windows(n)
         .enumerate()
         .find(|(_, chars)| all_differ(chars))
         .map(|(pos, chars)| pos + chars.len())
+}
+
+fn detect_sop(s: &str) -> Option<usize> {
+    detect_start_indicator(s, 4)
 }
 
 #[test]
@@ -37,10 +41,26 @@ fn test_detect_sop() {
     assert_eq!(detect_sop("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").unwrap(), 11);
 }
 
+fn detect_som(s: &str) -> Option<usize> {
+    detect_start_indicator(s, 14)
+}
+
+#[test]
+fn test_detect_som() {
+    assert_eq!(detect_som("bvwbjplbgvbhsrlpgdmjqwftvncz").unwrap(), 19);
+    assert_eq!(detect_som("nppdvjthqldpwncqszvftbrmjlhg").unwrap(), 23);
+    assert_eq!(detect_som("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").unwrap(), 23);
+    assert_eq!(detect_som("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").unwrap(), 29);
+}
+
 fn main() {
     let text = str::from_utf8(include_bytes!("input.txt")).expect("valid input file");
     println!(
         "Day 06 part 1: {}",
         detect_sop(text).expect("puzzle input has no SOP sequence")
+    );
+    println!(
+        "Day 06 part 2: {}",
+        detect_som(text).expect("puzzle input has no SOM sequence")
     );
 }
