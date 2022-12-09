7b5965e9b24fc2a6dcfea8551c6e8d0184e89e30
James Youngman <james@youngman.org>
2022-12-09 16:50:17 +0000

    Solved Day 09 part 2.

--- aor2022/src/bin/day09/main.rs
+++ aor2022/src/bin/day09/main.rs
@@ -10,17 +10,74 @@ const MAX_V_GAP: i64 = 1;
 
 #[derive(Debug, Clone)]
 struct Rope {
-    head: Position,
-    tail: Position,
+    knots: Vec<Position>,
 }
 
-impl Default for Rope {
-    fn default() -> Rope {
-        let origin = Position { x: 0, y: 0 };
-        Rope {
-            head: origin,
-            tail: origin,
+fn close_enough(head_pos: &Position, tail_pos: &Position) -> bool {
+    let hgap = head_pos.x - tail_pos.x;
+    let vgap = head_pos.y - tail_pos.y;
+    if hgap.abs() <= MAX_H_GAP {
+        if vgap.abs() <= MAX_V_GAP {
+            true
+        } else {
+            false
         }
+    } else {
+        false
+    }
+}
+
+fn maybe_move_tail(head_pos: Position, tail_pos: Position) -> Position {
+    if close_enough(&head_pos, &tail_pos) {
+        tail_pos
+    } else {
+        let result = Position {
+            x: tail_pos.x + (head_pos.x - tail_pos.x).signum(),
+            y: tail_pos.y + (head_pos.y - tail_pos.y).signum(),
+        };
+        // we want to re-establish this invariant
+        assert!(close_enough(&head_pos, &result));
+        result
+    }
+}
+
+fn update_knot_position(head: &Position, tail: &mut Position) {
+    *tail = maybe_move_tail(*head, *tail);
+}
+
+fn visit_pairs<T>(v: &mut [T], mut f: impl FnMut(&T, &mut T)) {
+    let mut prev: Option<&T> = None;
+    for item in v.iter_mut() {
+        if let Some(previous) = prev {
+            f(previous, item);
+        }
+        prev = Some(&*item);
+    }
+}
+
+impl Rope {
+    fn new(len: usize) -> Rope {
+        Rope {
+            knots: vec![Position { x: 0, y: 0 }; len],
+        }
+    }
+
+    fn all_close_enough(&self) -> bool {
+        self.knots
+            .iter()
+            .fold((true, None), |(good, prev), knot| match prev {
+                Some(previous_knot) => (good && close_enough(previous_knot, knot), Some(knot)),
+                None => (good, Some(knot)),
+            })
+            .0
+    }
+
+    /// Move the head of the rope and, if necessary, the tail.
+    fn perform_move(&mut self, head_direction: CompassDirection) {
+        assert!(self.all_close_enough()); // invariant
+        self.knots[0] = self.knots[0].move_direction(&head_direction);
+        visit_pairs(&mut self.knots, update_knot_position);
+        assert!(self.all_close_enough()); // invariant
     }
 }
 
@@ -60,46 +117,6 @@ impl TryFrom<&str> for Move {
     }
 }
 
-fn close_enough(head_pos: &Position, tail_pos: &Position) -> bool {
-    let hgap = head_pos.x - tail_pos.x;
-    let vgap = head_pos.y - tail_pos.y;
-    if hgap.abs() <= MAX_H_GAP {
-        if vgap.abs() <= MAX_V_GAP {
-            true
-        } else {
-            false
-        }
-    } else {
-        false
-    }
-}
-
-fn maybe_move_tail(head_pos: Position, tail_pos: Position) -> Position {
-    if close_enough(&head_pos, &tail_pos) {
-        tail_pos
-    } else {
-        let result = Position {
-            x: tail_pos.x + (head_pos.x - tail_pos.x).signum(),
-            y: tail_pos.y + (head_pos.y - tail_pos.y).signum(),
-        };
-        // we want to re-establish this invariant
-        assert!(close_enough(&head_pos, &result));
-        result
-    }
-}
-
-/// Move the head of the rope and, if necessary, the tail.
-fn perform_move(current: Rope, head_direction: CompassDirection) -> Rope {
-    assert!(close_enough(&current.head, &current.tail)); // invariant
-    let head_pos = current.head.move_direction(&head_direction);
-    let tail_pos = maybe_move_tail(head_pos, current.tail);
-    assert!(close_enough(&head_pos, &tail_pos)); // invariant
-    Rope {
-        head: head_pos,
-        tail: tail_pos,
-    }
-}
-
 fn perform_sequence<I: IntoIterator<Item = Move>>(start: Rope, moves: I) -> Vec<Rope> {
     struct State {
         current: Rope,
@@ -112,7 +129,7 @@ fn perform_sequence<I: IntoIterator<Item = Move>>(start: Rope, moves: I) -> Vec<
                 mut history,
             } => {
                 for _ in 0..thismove.count {
-                    current = perform_move(current, thismove.direction);
+                    current.perform_move(thismove.direction);
                     history.push(current.clone());
                 }
                 State { current, history }
@@ -135,14 +152,22 @@ fn parse_moves(s: &str) -> Result<Vec<Move>, Fail> {
     s.split_terminator('\n').map(Move::try_from).collect()
 }
 
-fn count_unique_tail_positions(moves: Vec<Move>) -> usize {
-    let history = perform_sequence(Rope::default(), moves);
-    let positions: HashSet<Position> = history.into_iter().map(|rope| rope.tail).collect();
+fn count_unique_tail_positions(moves: Vec<Move>, len: usize) -> usize {
+    let history = perform_sequence(Rope::new(len), moves);
+    let positions: HashSet<Position> = history
+        .into_iter()
+        .map(|mut rope| rope.knots.pop())
+        .flatten()
+        .collect();
     positions.len()
 }
 
 fn solve_part1(s: &str) -> Result<usize, Fail> {
-    Ok(count_unique_tail_positions(parse_moves(s)?))
+    Ok(count_unique_tail_positions(parse_moves(s)?, 2))
+}
+
+fn solve_part2(s: &str) -> Result<usize, Fail> {
+    Ok(count_unique_tail_positions(parse_moves(s)?, 10))
 }
 
 #[test]
@@ -150,7 +175,7 @@ fn test_count_unique_tail_positions() {
     const EXAMPLE: &str =
         concat!("R 4\n", "U 4\n", "L 3\n", "D 1\n", "R 4\n", "D 1\n", "L 5\n", "R 2\n",);
     let moves = parse_moves(EXAMPLE).expect("example should be valid");
-    assert_eq!(count_unique_tail_positions(moves), 13);
+    assert_eq!(count_unique_tail_positions(moves, 2), 13);
 }
 
 fn main() {
@@ -158,5 +183,9 @@ fn main() {
     println!(
         "Day 09 part 1: {}",
         solve_part1(text).expect("should be able to solve part 1")
+    );
+    println!(
+        "Day 09 part 1: {}",
+        solve_part2(text).expect("should be able to solve part 2")
     );
 }
